d69d8d30494fb19b7479cae8115fb4fa91e7c7d0
Pralay Mondal <63528973+proComp11@users.noreply.github.com>
2022-12-02 16:06:25 +0530

    Create uppercase.py

---
+++ uppercase.py
@@ -0,0 +1,5 @@
+# Uppercase first and last character of a string
+_str1 = 'python programming language'
+_strLen = len(_str1)
+print(_str1)
+print(_str1[0].upper()+ _str1[1:_strLen-1] + _str1[-1].upper())
