553578ddec82437a8a2f0ac5cef8b16fce8d34d8
Pralay Mondal <63528973+proComp11@users.noreply.github.com>
2022-12-02 14:24:15 +0530

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,8 @@
+#Check if a string contains a specific substring
+str1 = "python progamming is very easy"
+str2 = "progamming"
+#print(str2 in str1)
+if str2 in str1:
+    print(str1 + ' contains the substring ' + str2)
+else:
+    print(str1 + ' does\'t contains the substring ' + str2)
